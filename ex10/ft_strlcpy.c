8cb0c4e5bb98213b3ef7598ee580b14ee39d575f
Theo SIGNORE <tsignore@e2r2p22.42.fr>
2020-07-07 18:22:00 +0200

    ex10 done after accidentaly deleting it - ready to submit

--- ex10/ft_strlcpy.c
+++ ex10/ft_strlcpy.c
@@ -5,25 +5,33 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: tsignore <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2020/07/07 14:56:46 by tsignore          #+#    #+#             */
-/*   Updated: 2020/07/07 15:57:22 by tsignore         ###   ########.fr       */
+/*   Created: 2020/07/07 17:49:16 by tsignore          #+#    #+#             */
+/*   Updated: 2020/07/07 18:20:34 by tsignore         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+unsigned int	ft_strlen(char *str)
+{
+	unsigned int i;
+
+	i = 0;
+	while (str[i] != '\0')
+		i++;
+	return (i);
+}
 
 unsigned int	ft_strlcpy(char *dest, char *src, unsigned int size)
 {
 	unsigned int i;
-	unsigned int rvalue;
+	unsigned int src_len;
 
+	src_len = ft_strlen(src);
 	i = 0;
-	rvalue = 0;
-	while (src[rvalue] != '\0')
-		rvalue++;
-	while (i < size - 1 && i < rvalue)
+	while (i < size - 1 && i < src_len)
 	{
 		dest[i] = src[i];
 		i++;
 	}
-	dest[size - 1] = size != 0 ? '\0' : dest[size - 1];
-	return (rvalue);
+	dest[size - 1] = '\0';
+	return (src_len);
 }
