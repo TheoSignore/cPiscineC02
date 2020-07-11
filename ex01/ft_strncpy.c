88fb8016e738c68927e6d4d5414e4b9b0dd22728
Theo SIGNORE <tsignore@e2r2p22.42.fr>
2020-07-11 09:51:13 +0200

    ex01 must be good this time

--- ex01/ft_strncpy.c
+++ ex01/ft_strncpy.c
@@ -6,35 +6,25 @@
 /*   By: tsignore <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/07/07 10:10:48 by tsignore          #+#    #+#             */
-/*   Updated: 2020/07/09 17:26:42 by tsignore         ###   ########.fr       */
+/*   Updated: 2020/07/11 09:50:28 by tsignore         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-
-int		ft_strlen(char *str)
-{
-	int size;
-
-	size = 0;
-	while (str[size] != '\0')
-		size++;
-	return (size);
-}
 
 char	*ft_strncpy(char *dest, char *src, unsigned int n)
 {
 	unsigned int i;
-	unsigned int dst_len;
+	unsigned int src_len;
 
-	dst_len = ft_strlen(dest);
+	src_len = 0;
+	while (src[src_len])
+		src_len++;
 	i = 0;
-	while (i < n && src[i] && dest[i])
+	while (i < n)
 	{
-		dest[i] = src[i];
-		i++;
-	}
-	while (i <= n && i < dst_len)
-	{
-		dest[i] = '\0';
+		if (i > src_len)
+			dest[i] = '\0';
+		else
+			dest[i] = src[i];
 		i++;
 	}
 	return (dest);
